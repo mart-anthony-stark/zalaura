3e6b94c86486e46d804a1e324d2bcdc3326f6a4b
mart-anthony-stark <martsalazar0@gmail.com>
2024-03-06 14:17:54 +0800

    Add: padding top

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <main className="pt-20">{children}</main>
+        <main className="pt-24">{children}</main>
       </body>
     </html>
   );
