fd9b86d1bb59494a113e341a1a75d7f9d4aa6cad
mart-anthony-stark <martsalazar0@gmail.com>
2024-03-06 15:08:21 +0800

    CHANGE: TITLE

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <Link href='/' className="text-3xl font-light">ZALORA</Link>
+    <Link href='/' className="text-3xl font-light">ZALAURA</Link>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="flex gap-10 items-center justify-around">
           <Image src="/landing-page.jpg" width={400} height={500} alt="Shop" />
           <div className="flex flex-col gap-5">
-            <h1 className="text-5xl font-bold uppercase">Zalora</h1>
+            <h1 className="text-5xl font-bold uppercase">Zalaura</h1>
             <Link href="/products">
               <Button className="w-full">Shop Now</Button>
             </Link>
