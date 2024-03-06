91aacc903e367d4eb4c2faef93db4b5c2832825e
mart-anthony-stark <martsalazar0@gmail.com>
2024-03-06 14:18:09 +0800

    remove home

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-14">
       <div className="flex gap-4">
-        <h1>Home</h1>
-
         <div>
           <Link href="/products">Products</Link>
         </div>
